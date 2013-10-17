e97417ca66dbd8bd68589eba2750a4abc34223e7
Sebastian Schildt <sebastian@frozenlight.de>
2013-10-17 22:19:39 +0200

    Switch to current day automatically

--- MensaBS/MainPage.xaml.cs
+++ MensaBS/MainPage.xaml.cs
@@ -45,6 +45,13 @@ namespace MensaBS
 
         private void appReady(object src, RoutedEventArgs e)
         {
+            DateTime now = DateTime.Now;
+/*            switch (now.DayOfWeek)
+            {
+                case 0: mainGuiThingie.SelectedIndex = 0;
+            }*/
+            mainGuiThingie.SelectedIndex = (int)now.DayOfWeek;
+
             load_kath(null,null);
         }
 
